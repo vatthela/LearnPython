4d8fbae8551e7642f67592f2cecb9f4980ed003b
vatthela <45648067+vatthela@users.noreply.github.com>
2021-09-18 09:32:09 +0700

    commit

---
+++ hackathon1_midterm/easy.py
@@ -0,0 +1,7 @@
+# 1
+def day_diff(release_date, code_complete_day):
+    pass
+
+# 2
+def alpha_num(sentence):
+    pass
---
+++ hackathon1_midterm/medium.py
@@ -0,0 +1,5 @@
+def anagram_number(number):
+    pass
+
+def roman_to_int(s):
+    pass
